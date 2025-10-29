1f673d58075ab636a93c8fee071489b1fbcef80b
qyrhx <qyrhx@disroot.org>
2025-10-29 18:22:14 +0100

    Started Sudoku Solver

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Haskell
+Going through the book of Professor Graham Hutton [Programming in Haskell](https://people.cs.nott.ac.uk/pszgmh/pih.html),
+as well as the lectures on Youtube (links in the book's page).
---
+++ sudoku-solver.hs
@@ -0,0 +1,38 @@
+-- λ
+
+import Data.List (transpose)
+
+type Grid = Matrix Value
+type Matrix a = [Row a]
+type Row a = [a]
+type Value = Int
+
+emptyGrid :: Grid
+emptyGrid = replicate 9 [1..9]
+--emptyGrid = replicate 9 $ replicate 9 ' '
+
+rows :: Matrix a -> [Row a]
+rows = id
+
+cols :: Matrix a -> [Row a]
+cols = transpose
+--cols m = map (\i -> map (!! i) m) [0..8]
+
+boxes :: Matrix a -> [Row a]
+boxes m = concatMap boxesOfTriplet $ triplets m where
+  -- Note: could easily use `chunksOf` from `split` package
+  -- but I don't want to add external deps lol
+  triplets :: [a] -> [[a]]
+  triplets [] = []
+  triplets l = triplet : triplets rest where (triplet, rest) = splitAt 3 l
+  -- Takes three rows, returns three boxes
+  boxesOfTriplet :: [Row a] -> [Row a]
+  boxesOfTriplet threeRs = [concatMap (take 3 . drop (i*3)) threeRs | i <- [0..2]]
+
+valid :: Grid -> Bool
+valid g =
+  all nodups (rows g) &&
+  all nodups (cols g) &&
+  all nodups (boxes g)
+  where nodups [] = True
+        nodups (x:xs) = notElem x xs && nodups xs
